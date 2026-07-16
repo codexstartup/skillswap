a9a2af9fd6fffaf6ab637ee605c111a929379372
codexstartup <therooted.com@gmail.com>
2026-07-16 20:32:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const themeBtn = document.getElementById("themeBtn");
 
 themeBtn.addEventListener("click", () => {
+
     document.body.classList.toggle("dark");
 
-    if (document.body.classList.contains("dark")) {
+    if(document.body.classList.contains("dark")){
         themeBtn.innerHTML = "☀️";
-    } else {
-        themeBtn.innerHTML = "🌙";
+        localStorage.setItem("theme", "dark");
     }
+    else{
+        themeBtn.innerHTML = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+
 });
+
+
+// Remember theme
+
+if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark");
+    themeBtn.innerHTML = "☀️";
+
+}
